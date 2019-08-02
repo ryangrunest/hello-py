91b0471d8925d70c2921f7c3c06202952763bdc3
Ryan Grunest <ryangrunest45@gmail.com>
2019-08-02 15:40:56 -0700

    first commit

---
+++ schema.py
@@ -0,0 +1,21 @@
+import graphene
+import json
+
+class Query(graphene.ObjectType):
+  hello = graphene.String()
+
+  def resolve_hello(self, info):
+    return "world"
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+  '''
+  {
+    hello
+  }
+  '''
+)
+
+dictResult = dict(result.data.items())
+print(json.dumps(dictResult, indent=2))
